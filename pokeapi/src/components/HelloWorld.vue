c2ba03c6e422a8f5daff7f5342f8d27fc0db3d74
Dylan Trepos <dylan.trepos@gmail.com>
2022-05-27 22:43:28 +0200

    (style) Add rotating animation

--- pokeapi/src/components/HelloWorld.vue
+++ pokeapi/src/components/HelloWorld.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>{{ pokemon?.name }}</h1>
-  <div @mouseover="pokemonImage = pokemon?.sprites?.back_default" @mouseleave="pokemonImage = pokemon?.sprites?.front_default">
-    <img 
-    :src="pokemonImage" 
-    alt="" 
-    class="img-result"
-    
-    >
+  <div 
+    class="box-image">
+      <img 
+      :src="pokemonImage?.front_default" 
+      class="img-result img-front"
+      ref="imgFront"
+      >
+      <img 
+      :src="pokemonImage?.back_default" 
+      class="img-result img-back"
+      ref="imgBack"
+      >
   </div>
   <div class="box-searchPokemon">
     <form @submit.prevent="callPokemon">
@@ -38,7 +43,6 @@
 </template>
 
 <script>
-  import pokemon from 'pokemon'
 
   export default {
     data() {
@@ -49,6 +53,7 @@
         pokemonName: 'pikachu',
         pokemonsEvolution: [],
         listPokeNames: [],
+        imageShow: null,
         pokemonImage: null,
         error: false,
       }
@@ -60,13 +65,17 @@
         try {
           this.error = false;
           if(this.pokemon?.name !== this.pokemonName){  
-            this.pokemon = await fetch(`${this.url}/${this.pokemonName}`).then((data) => data.json())
-            this.pokemonImage = this.pokemon.sprites.front_default
+            this.pokemon = await fetch(`${this.url}/${this.pokemonName}`).then((data) => data.json());
+            this.pokemonImage = await this.pokemon.sprites;
+
+            this.imageShow = this.pokemonImage.front_default;
+            this.$refs.imgBack.classList.remove('anim-img-enter');
+            this.$refs.imgFront.classList.add('anim-img-enter');
+            
             const link = this.pokemon.species.url;
             this.pokemonEvolutionLink = await fetch(link).then((data) => (data.json()))
             this.pokemonEvolutionData = await fetch(this.pokemonEvolutionLink.evolution_chain.url).then((data) => (data.json()))
             let path = this.pokemonEvolutionData.chain;
-            console.log(this.pokemonEvolutionData)
             this.pokemonsEvolution = [];
             this.pokemonsEvolution.push(fetch(`${this.url}/${path.species.name}`).then((data) => data.json()))
 
@@ -78,13 +87,25 @@
             } 
 
             this.pokemonsEvolution = await Promise.all(this.pokemonsEvolution)
-
             this.pokemonsEvolution = this.pokemonsEvolution.filter((pokemonEvo) => pokemonEvo.name !== this.pokemonName)
           }
         } catch(error) {
           this.error = true;
         }
-        console.log(this.pokemon)
+      },
+      animationImg() {
+          if(this.pokemonImage.back_default) {
+            if(this.imageShow === this.pokemonImage.front_default) {
+              this.imageShow = this.pokemonImage.back_default;
+              this.$refs.imgFront.classList.remove('anim-img-enter');
+              this.$refs.imgBack.classList.add('anim-img-enter');
+            }
+            else {
+              this.imageShow = this.pokemonImage.front_default;
+              if(this.$refs.imgBack) this.$refs.imgBack.classList.remove('anim-img-enter');
+              this.$refs.imgFront.classList.add('anim-img-enter');
+            }
+          } 
       },
       generatePokemon() {
         function randomInRange(min, max)
@@ -92,9 +113,7 @@
           return Math.floor(Math.random() * (max - min + 1) + min);
         }
         const randomR = randomInRange(0, 1126);
-        console.log('r :', randomR)
         const myArr = this.listPokeNames
-        console.log('ici : ', this.listPokeNames[0])
         this.pokemonName = this.listPokeNames[randomR].name
       },
       switchPokemon(newPokemon) {
@@ -111,6 +130,7 @@
           this.listPokeNames = data.results;
       }
       test()
+      setInterval(this.animationImg, 3000)
     },
   }
 </script>
